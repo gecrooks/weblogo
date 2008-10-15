f6c7f214f720152b3352e3c0c41b16c257000ff8
GavinCrooks <GavinCrooks@62591b3d-121e-0410-be2a-b73053c84cf5>
2008-10-15 18:01:19 +0000

    m

--- trunk/setup.py
+++ trunk/setup.py
@@ -31,7 +31,8 @@ def main() :
         maintainer       = "Gavin Crooks",
         maintainer_email = "gec@threeplusone.com",
         url              = "http://code.google.com/p/weblogo/",    
-        download_url     = 'http://weblogo.googlecode.com/svn/dist/weblogo-%s.tar.gz' % __version__ ,
+        
+        download_url     = 'http://weblogo.googlecode.com/files/weblogo-%s.tar.gz' % __version__ ,
         classifiers      =[
             'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Science/Research',
