ae63b10b7e464b6a48b5ad5d435bef2d011ef9e8
gavincrooks <gavincrooks@gmail.com>
2011-10-11 04:05:24 +0000

    Update version number to 3.2

--- weblogolib/__init__.py
+++ weblogolib/__init__.py
@@ -153,7 +153,7 @@ __all__ = [ 'LogoOptions',
 
 description  = "Create sequence logos from biological sequence alignments." 
 
-__version__ = "3.1"
+__version__ = "3.2"
 
 # These keywords are subsituted by subversion.
 # The date and revision will only tell the truth after a branch or tag,
