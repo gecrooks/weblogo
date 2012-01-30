f21677afef657277b9d517686f4c3798070aff95
gavincrooks <gavincrooks@gmail.com>
2012-01-30 07:43:58 +0000

    Cleanup in preperation for release

--- corebio/_version.py
+++ corebio/_version.py
@@ -10,7 +10,7 @@ revision = "$Revision: 170 $".split()[1]
 # The patch level should be zero in trunk, a positive number in a release 
 # branch. During a release, increment the minor number in trunk and set the
 # patch level to 1 in the branch. Increment patch number for bug fix releases.
-__version__ = '0.6w' + revision 
+__version__ = '3.2' + revision 
 
 
 description = "CoreBio %s (%s)" % (__version__,  date)
