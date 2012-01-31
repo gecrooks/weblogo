675d9d4456a3d8dccbb1b977cddeed6e29fa709a
gavincrooks@gmail.com <gavincrooks@gmail.com@62591b3d-121e-0410-be2a-b73053c84cf5>
2012-01-31 01:55:21 +0000

    Commit to get versions and keyword subsitutions correct for 3.2 branch

--- branches/3.2/weblogolib/__init__.py
+++ branches/3.2/weblogolib/__init__.py
@@ -155,7 +155,7 @@ description  = "Create sequence logos from biological sequence alignments."
 
 __version__ = "3.2"
 
-# These keywords are substituted by subversion.
+# These keywords are substituted by subversion .
 # The date and revision will only tell the truth after a branch or tag,
 # since different files in trunk will have been changed at different times
 release_date ="$Date$".split()[1]
