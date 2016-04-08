888573a80e7a2ff374e9cb6c4d3a90939dbb899c
GEC <gec@threeplusone.com>
2016-04-08 12:58:36 -0700

    Fix minor bug that showed up as deprecation warning in python 3.5

--- weblogolib/logomath.py
+++ weblogolib/logomath.py
@@ -111,7 +111,7 @@ class Dirichlet(object) :
         return theta
 
     def mean(self) :
-        if  self._mean ==None:
+        if  self._mean is None:
             self._mean = self.alpha / self._total
         return self._mean
     
