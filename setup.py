d392442f6031ca15692d1a9d18ffb830e062e23b
m.fab <go-bears@users.noreply.github.com>
2017-12-27 20:31:47 -0800

    update setup.py to import version string fr corebio

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 #warnings.simplefilter('ignore', UserWarning, lineno=236)
 
 
-from weblogolib import __version__
+from corebio._version import __version__
 
 
 def main():
