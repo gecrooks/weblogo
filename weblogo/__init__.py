618498ec742572888a9ee8ca5d8937a8c5368421
gecrooks <gec@threeplusone.com>
2019-03-04 09:03:34 -0800

    Fixup versioning

--- weblogo/__init__.py
+++ weblogo/__init__.py
@@ -1,6 +1,11 @@
 
+
 from setuptools_scm import get_version
-__version__ = get_version(root='..', relative_to=__file__)
+
+try:
+    __version__ = get_version(root='..', relative_to=__file__)
+except:
+    __version__ = '?.?.?'
 
 from .logo import *								# noqa: F401, F403
 from .seq import *								# noqa: F401, F403
