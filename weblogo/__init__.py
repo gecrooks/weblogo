e9b9e608cce5bdef1c76a3577049346ef9edf555
go-bears <melissa.fabros@gmail.com>
2021-12-27 18:27:11 -0800

    add str type to __version__

--- weblogo/__init__.py
+++ weblogo/__init__.py
@@ -10,10 +10,3 @@ from .logo import *  # noqa: F401, F403
 from .logo_formatter import *  # noqa: F401, F403
 from .seq import *  # noqa: F401, F403
 from .seq_io import *  # noqa: F401, F403
-
-__version__: str
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
-    __version__ = "?.?.?"
