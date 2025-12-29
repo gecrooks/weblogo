4b6bd74d62acec6b3ed3e2f12635553db3a16cc7
Gavin Crooks <gecrooks@users.noreply.github.com>
2025-12-28 20:43:04 -0800

    Change install to uv from pip'

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ html_sidebars = {
 
 
 def setup(app):
-    app.add_stylesheet("qf.css")
+    app.add_css_file("qf.css")
 
 
 # -- Project information -----------------------------------------------------
@@ -80,7 +80,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
